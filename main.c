e52b6b678a34077362eaf51104a0c72d899d85d1
nahuelaceval <nahuel.acevalr@gmail.com>
2017-03-23 21:36:27 -0300

    adasdas

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+
+int main()
+{
+int respuesta;
+
+    respuesta=obtenerInt("ingrese un numero\n","Error, reingrese un numero entre 0 y 100 :\n",100,0);
+    printf("Su edad es: %d",respuesta);
+
+    return 0;
+}
+
